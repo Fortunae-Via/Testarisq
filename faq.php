4c14447f0560f8babddf7375290c4b06d59b8161
jiemingxuxuxu <74723376+jiemingxuxuxu@users.noreply.github.com>
2020-12-11 16:20:58 +0100

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>TESTARISQ - Faq</title> 
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style/faq_style.php"/>
+</head>
+
+<body>
+    <!-- Header -->
+    <?php include("php/header.php"); ?>
+    <hr>
+    <div class="topnav">
+        <p1><b>Les questions reviennent souvent:</b></p1>
+    </div>
+    <div class="q1">
+        <div class="dropdown">
+            <span><p><b>Première Question?</b></p></span>
+            <div class="dropdown-content">
+                <p>Le concept de feuille de style est présent dès l'origine du World Wide Web :</p>
+                <p> le premier navigateur web WorldWideWeb permet de mettre en forme les documents </p>
+                <p>à l'aide de ce qui serait aujourd'hui considéré comme une « feuille de style utilisateur ».</p>
+                <p>De même, les navigateurs Viola en 1992 et Harmony en 1993 recourent à </p>
+                <p>un mécanisme similaire permettant de déterminer le rendu des polices de caractères, des couleurs ou de l'alignement du texte.</p>
+            </div>
+        </div>
+    </div>
+    <div class="q1">
+        <div class="dropdown">
+            <span><p><b>Demière Question?</b></p></span>
+            <div class="dropdown-content">
+                <p>Le concept de feuille de style est présent dès l'origine du World Wide Web :</p>
+                <p> le premier navigateur web WorldWideWeb permet de mettre en forme les documents </p>
+                <p>à l'aide de ce qui serait aujourd'hui considéré comme une « feuille de style utilisateur ».</p>
+                <p>De même, les navigateurs Viola en 1992 et Harmony en 1993 recourent à </p>
+                <p>un mécanisme similaire permettant de déterminer le rendu des polices de caractères, des couleurs ou de l'alignement du texte.</p> 
+            </div>
+        </div>
+    </div>
+    <div class="q1">
+        <div class="dropdown">
+            <span><p><b>Troisième Question?</b></p></span>
+            <div class="dropdown-content">
+                <p>Le concept de feuille de style est présent dès l'origine du World Wide Web :</p>
+                <p> le premier navigateur web WorldWideWeb permet de mettre en forme les documents </p>
+                <p>à l'aide de ce qui serait aujourd'hui considéré comme une « feuille de style utilisateur ».</p>
+                <p>De même, les navigateurs Viola en 1992 et Harmony en 1993 recourent à </p>
+                <p>un mécanisme similaire permettant de déterminer le rendu des polices de caractères, des couleurs ou de l'alignement du texte.</p>
+            </div>
+        </div>
+    </div>
+    <div class="topnav">
+        <p1>Vous ne trouvez pas de réponse à votre question?</p1>
+        <br>
+        <p1>Posez là directement à un administrateur via<a href="#site d'un administrateur"><p2> ce formulaire</p2></a></p1>
+    </div>
+
+</body>
+</html>
