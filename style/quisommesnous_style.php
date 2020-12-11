1af42e836765618081bdd7cc8193c0328f9b24b8
jiemingxuxuxu <74723376+jiemingxuxuxu@users.noreply.github.com>
2020-12-11 15:54:57 +0100

    Add files via upload

---
+++ style/quisommesnous_style.php
@@ -0,0 +1,42 @@
+<?php header("Content-type: text/css"); ?>
+
+/* Style commun à toutes les pages */
+<?php include("style_commun.php"); ?>
+
+/*quisommesnous*/
+
+div
+{
+    margin-top: 25px;
+    margin-bottom: 25px;
+    margin-left: 50px;
+    margin-right: 50px;
+    text-align: center;
+    padding: 1px;
+}
+.topnav {
+  overflow: hidden;
+  text-align: center;
+}    
+p1
+{
+    color: #ffffff;
+    font-size: 150%;
+}
+
+.content
+{
+    border-style: solid;
+    text-align: center;
+    background-color: rgb(251, 255, 255);
+    width: 1100px;
+    height: 500px;
+    margin: auto;
+    
+}
+p2:link{color: #3388BB;}
+.foot
+{
+    vertical-align: bottom;
+    text-align: center;
+}
