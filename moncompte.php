c935d0193eda8bd0052a2f8c88697f54596dd419
TanguyRo <52966403+TanguyRo@users.noreply.github.com>
2020-12-16 21:44:27 +0100

    Add files via upload

--- moncompte.php
+++ moncompte.php
@@ -2,15 +2,13 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-    <link rel="stylesheet" href="style/style_commun.css" />
-    <link rel="stylesheet" href="style/header.css" />
-    <link rel="stylesheet" href="style/MonCompte.css" />
+    <link rel="stylesheet" href="style/MonCompte_style.php" />
 	<title>TESTARISQ - Mon Compte</title>
 </head>
 <body>
 
 	<!-- Header -->
-	<?php include("php/header.php"); ?>
+	<?php include("php/header.php");?>
 	
 	<div class="div_page">
 		<header>
@@ -18,7 +16,30 @@
         </header>
 		<section id = "sect">
 			<div class="infos">
-				<p>Nom de famille :<span class="user_info">Dupont</span></p>
+
+				<?php 
+				try
+		{
+		// On se connecte à MySQL
+		$bdd = new PDO('mysql:host=localhost;dbname=bdd_testarisq;charset=utf8', 'root', '');
+		
+		}
+		catch(Exception $e)
+		{
+			
+		// En cas d'erreur, on affiche un message et on arrête tout
+     	   die('Erreur : '.$e->getMessage());
+		}
+				/*$req = $bdd->prepare("INSERT INTO 'nom de la bdd' SET username = ?, password = ?, email = ?");
+				 --> dans la page register */
+				session_start();
+				$user = $bdd->query('select * from personne'); 
+				while ($data = $user->fetch())
+					{$prenom1 = $data['Premier prénom'];
+			$prenom2 = $data['Deuxième Prénom'];
+			$prenom3 = $data['Troisième Prénom'];}
+				?>
+				<p>Nom de famille :<span class="user_info">Dupont <?= $prenom1 ?> <?= $prenom2 ?> <?= $prenom3 ?></span></p>
 				<p>Nom d'usage :<span class="user_info"></span></p>
 				<p>Prénoms :<span class="user_info">Jean, Jacques, Paul</span></p>
 				<p>Né(e) le :<span class="user_info">01/01/2000</span></p>
