c2e92bb72033ed85c785c4992d01f73e072aff94
marionfr <74741185+marionfr@users.noreply.github.com>
2020-12-17 15:59:39 +0100

    Add files via upload

---
+++ Capteurs.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="style/capteurs_style.php" />
+	<title>TESTARISQ - Mes résultats</title>
+</head>
+<body>
+
+	<!-- Header -->
+	<?php include("php/header.php"); ?>
+
+<div class='conteneur'>
+  <section id='boitier_1'>
+  <h2>Boîtier 1</h2>
+  
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 1 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+  <form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 2 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 3 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 4 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 5 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+</section>
+
+<section id='boitier_2'>
+  <h2>Boîtier 2</h2>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 1 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+  <form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 2 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 3 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 4 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+<form method="post" action="traitement.php">
+   <p>
+       <label for="capteur">Capteur 5 :</label>
+       <select name="capteur" id="capteur">
+         <option value="temperature">Température</option>
+           <option value="son">Son</option>
+           <option value="lumiere">Lumière</option>
+           <option value="frequence_cardiaque">Fréquence cardiaque</option>
+           <option value="frequence_sonore">Fréquence sonore</option>
+         <option value="rien">---</option>
+       </select>
+   </p>
+</form></p>
+</div>
+
+</body>
