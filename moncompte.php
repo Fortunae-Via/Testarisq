cf237aca15895b3b272031983985489853ef4815
Tanguy Robilliard <tanguy.robilliard@gmail.com>
2020-12-17 20:25:12 +0100

    mon compte fin

--- moncompte.php
+++ moncompte.php
@@ -2,15 +2,13 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-    <link rel="stylesheet" href="style/style_commun.css" />
-    <link rel="stylesheet" href="style/header.css" />
-    <link rel="stylesheet" href="style/MonCompte.css" />
+    <link rel="stylesheet" href="style/MonCompte_style.php" />
 	<title>TESTARISQ - Mon Compte</title>
 </head>
 <body>
 
 	<!-- Header -->
-	<?php include("php/header.php"); ?>
+	<?php include("php/header.php");?>
 	
 	<div class="div_page">
 		<header>
@@ -18,7 +16,32 @@
         </header>
 		<section id = "sect">
 			<div class="infos">
-				<p>Nom de famille :<span class="user_info">Dupont</span></p>
+
+				<?php 
+				try
+		{
+		// On se connecte à MySQL
+		$bdd = new PDO('mysql:host=localhost;dbname=bdd_testarisq;charset=utf8', 'root', '');
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+		
+		}
+		catch(Exception $e)
+		{
+			
+		// En cas d'erreur, on affiche un message et on arrête tout
+     	   die('Erreur : '.$e->getMessage());
+		}
+				/*$req = $bdd->prepare("INSERT INTO 'nom de la bdd' SET username = ?, password = ?, email = ?");
+				 --> dans la page register */
+				session_start();
+				$user = $bdd->query('select * from personne'); 
+				while ($data = $user->fetch())
+					{$prenom1 = $data['Premier prénom'];
+			$prenom2 = $data['Deuxième Prénom'];
+			$prenom3 = $data['Troisième Prénom'];}
+				?>
+				<p>Nom de famille :<span class="user_info">Dupont <?= $prenom1 ?> <?= $prenom2 ?> <?= $prenom3 ?></span></p>
 				<p>Nom d'usage :<span class="user_info"></span></p>
 				<p>Prénoms :<span class="user_info">Jean, Jacques, Paul</span></p>
 				<p>Né(e) le :<span class="user_info">01/01/2000</span></p>
@@ -27,7 +50,7 @@
 				<p>Téléphone portable :<span class="user_info">0607080910</span></p>
 				<p>Courriel :<span class="user_info">jean.dupont@isep.fr</span></p>
 			</div>
-			<p class="bottom">Un renseignement incorrect ? Signalez-le <a href="InformationIncorrecte.php">ici</a> à un administrateur.</p>
+			<p class="bottom">Un renseignement incorrect ? Signalez-le <a href="mailto:tanguy.robilliard@gmail.com">ici</a> à un administrateur.</p>
 		</section>
 	</div>
 	
