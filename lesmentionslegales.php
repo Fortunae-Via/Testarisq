e0143d6abd90b3d46f76a0553a277eb333448601
jiemingxuxuxu <74723376+jiemingxuxuxu@users.noreply.github.com>
2020-12-17 15:33:59 +0100

    Add files via upload

---
+++ lesmentionslegales.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>TESTARISQ - Les mentions legales</title> 
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style/style_commun.css" />
+<link rel="stylesheet" href="style/header.css" />
+<link rel="stylesheet" href="style/quisommesnous.css" />
+</head>
+
+<body>
+
+	<!-- Header -->
+    <?php include("php/header.php"); ?>
+    
+    <div class="div_page">
+        <header>
+            <h2>Les Mentions Légales</h2>
+        </header>
+        <div class="content">
+        　　<p>En vigueur au 18/12/2020</P>
+            <p>Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs du site <a href="#">www.fortunaevia.com</a> les présentes mentions légales.</p>
+            <p>La connexion et la navigation sur le site (indiquer le nom du site) par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.</p>
+            <p>Ces dernières sont accessibles sur le site à la rubrique « Mentions légales ».</p>
+            <br>
+            <h2>ARTICLE 1 : L’éditeur</h2>
+            <p>L’édition et la direction de la publication du site <a href="#">www.fortunaevia.com</a> est assurée par xxx, domiciliée  28 Rue Notre Dame des Champs, 75006 Paris, dont le numéro de téléphone est 0607080910, et l'adresse e-mail fortunae.via@fortunaevia.fr.</p>
+            <h2>ARTICLE 2 : L’hébergeur</h2>
+            <p>L'hébergeur du site <a href="#">www.fortunaevia.com</a> est la Société xxx, dont le siège social est situé au  28 Rue Notre Dame des Champs, 75006 Paris , avec le numéro de téléphone : 0607080910.</p>
+            <h2>ARTICLE 3 : Accès au site</h2>
+            <p>Le site est accessible par tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.</p>
+            <p>En cas de modification, interruption ou suspension des services le site <a href="#">www.fortunaevia.com</a> ne saurait être tenu responsable.</p>
+            <h2>ARTICLE 4 : Collecte des données</h2>
+            <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro 0607080910.</p>
+            <p>En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :</p>
+            <p>par mail à l'adresse mail :fortunae.via@fortunaevia.fr</p>
+            <p> via son espace personnel ;</p>
+            <h2> ARTICLE 5 : Cookies</h2>
+            <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
+            <p>En naviguant sur le site, il les accepte.</p>
+            <p>Un cookie est un élément qui ne permet pas d’identifier l’Utilisateur mais sert à enregistrer des informations relatives à la navigation de celui-ci sur le site Internet. L’Utilisateur pourra désactiver ce cookie par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
+            <h2>ARTICLE 6 : Propriété intellectuelle</h2>
+            <p>Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du site<a href="#"> www.fortunaevia.com</a>,  sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.</p>
+            
+            <p class="liens">Vous pouvez trouver Qui-sommes-nous<a href="quisommesnous.php"> ici</a> et nos CGU<a href="CGU.php"> ici</a></p> 
+        </div>
+    </div>
+</body>
+
+</html>    
