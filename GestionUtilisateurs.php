60da91c96db81ae373444452ad53361cec382d09
Axel M <axelmannessiez@yahoo.fr>
2020-12-27 10:47:37 +0100

    Protections des pages
    On vérifie sur les pages que l'utilisateur ait bien le type de compte nécessaire pour qu'il ne puisse pas accéder à des régions non-autorisées pour lui via l'url

--- GestionUtilisateurs.php
+++ GestionUtilisateurs.php
@@ -5,6 +5,10 @@ session_start();
 if (!(isset($_SESSION['NIR']))) {
 	header('Location: Accueil.php');
 }
+//S'il est connecté mais qu'il charge des pages non autorisées pour son type de compte on le renvoie à l'accueil
+else if ( $_SESSION['TypeCompte']!='ADM' ) {	
+	header('Location: Accueil.php');
+}
 
 ?>
 
