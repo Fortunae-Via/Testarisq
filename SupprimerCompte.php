8fb615a04b8bdaabaddcf7569091d816675dcdd0
lou025 <wrallday@gmail.com>
2021-01-18 23:11:59 +0100

    Supprimer Compte

---
+++ SupprimerCompte.php
@@ -0,0 +1,28 @@
+<?php
+// Si on a bien récupérer l'identifiant d'un utilisateur à la page précédente Alors :
+if(isset($_GET['NIR'])){
+	// Appel  de la base de donnée
+	require("modele/connexionbdd.php");
+
+
+	/**
+	Suppression d'un compte où l'identifiant unique correspond avec la valeur
+	du $_GET['NIR']
+	Puis
+	Suppression des données correspondantes dans la table adresse lié à 
+	la table personne par une clé étagère.
+	**/
+	$supprimer = $bdd->prepare('DELETE FROM compte WHERE Personne_NIR=?');
+	$supprimer->execute(array($_GET['NIR']));
+	$supprimer->closeCursor();
+
+	sleep(1);
+	if('1'){
+		/**
+		L'utilisateur est redirigé vers la page de recherche.
+		(Retour à la page précédente)
+		**/
+		header('Location: GestionUtilisateurs.php');
+	}
+}
+?>
