e12d67de7ada79333aa9bb71b98c5066ec695bee
Tanguy Robilliard <tanguy.robilliard@gmail.com>
2021-01-20 21:31:44 +0100

    htaccess

--- app_site_2/vues/AccueilAdministrateur.php
+++ app_site_2/vues/AccueilAdministrateur.php
@@ -25,8 +25,9 @@
             <div><a href="admin/GestionUtilisateurs.php">Gestion des utilisateurs</a></div>
             <div><a href="admin/GestionFAQ.php">Gestion de la F.A.Q.</a></div>
             <div><a href="admin/GestionBoitiers.php">Gestion des boîtiers Testarisq</a></div>
-            <div><a href="admin/GestionAutoritesResponsables.php">Gestion des autorités responsables</a></div>
+            <div><a href="admin/GestionAutoritesResponsables.php">Gestion des autorités responsables</a></div>            
             <div><a href="#">Test de Contrôle</a></div>
+            <div><a href="admin/ajout_htaccess.php">Ajout administrateur htaccess</a></div>
         </div>
 
     </div>
