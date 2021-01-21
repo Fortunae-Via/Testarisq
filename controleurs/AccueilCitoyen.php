0ecca903fa9ffc10e907e90f75d571e55b38cbae
marionfr <74741185+marionfr@users.noreply.github.com>
2021-01-21 23:42:37 +0100

    Update AccueilCitoyen.php

--- controleurs/AccueilCitoyen.php
+++ controleurs/AccueilCitoyen.php
@@ -2,6 +2,11 @@
 
 $Prenom1=$_SESSION['Infos']['Prenom1'];
 
-//Récupérer les infos sur les derniers tests passés etc
+require 'modele/RequetesTest.php';
+require 'modele/connexionbdd.php';
 
+$requete = RequeteDerniersTestsPersonne($bdd,$_SESSION['NIR']);
+
+
+//Récupérer les infos sur les derniers tests passés etc
 require 'vues/AccueilCitoyen.php'; 
